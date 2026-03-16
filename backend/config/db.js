3488503331370c9e561bf718b2923630ec2fbb2a
techentia-work <prabhjotwork2004@gmail.com>
2026-03-16 18:25:13 +0530

    server update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            // These options are no longer necessary in Mongoose 6+, but good for backwards compatibility
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
+          
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
